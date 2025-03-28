05e04d8910c2bd7103902b9dcd435b1b4005db66
Victor Chidubem Mazeli <52371324+victormazeli@users.noreply.github.com>
2025-03-28 08:34:36 +0100

    fixes

--- proctored_exam/backend/models/flaggedActivity.js
+++ proctored_exam/backend/models/flaggedActivity.js
@@ -2,22 +2,22 @@ const mongoose = require('mongoose');
 
 const FlaggedActivitySchema = new mongoose.Schema({
   attemptId: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'Attempt',
     required: true
   },
   userId: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
   examId: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'Exam',
     required: true
   },
   certificationId: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'Certification'
   },
   reason: {
@@ -47,14 +47,14 @@ const FlaggedActivitySchema = new mongoose.Schema({
     default: Date.now
   },
   createdBy: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   },
   resolvedAt: {
     type: Date
   },
   resolvedBy: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   }
 });
